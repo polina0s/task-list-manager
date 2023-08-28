a717dbf16aa9c896fb29267e26a78f2e109d83fb
polina0s <polina.osintseva000@gmail.com>
2023-08-28 18:04:22 +0400

    add eslint-plugin-prettier, add new script 'prettier'

--- task-manager/.eslintrc.cjs
+++ task-manager/.eslintrc.cjs
@@ -10,19 +10,19 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'prettier', "simple-import-sort"],
+  plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'prettier/prettier': 'error',
-    'quotes': ['error', 'single'],
+    quotes: ['error', 'single'],
     'no-dupe-args': 'error',
     'no-extra-semi': 'error',
     'no-unused-vars': 'error',
-    "react/prop-types": 'off',
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error"
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
-}
+};
--- task-manager/src/main.jsx
+++ task-manager/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
-console.log(import.meta.env.VITE_LINK)
+);
+console.log(import.meta.env.VITE_LINK);
