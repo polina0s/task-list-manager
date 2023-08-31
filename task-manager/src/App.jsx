e72b7a497eb37b0ec93a112a93324e20466f7844
polina0s <polina.osintseva000@gmail.com>
2023-08-31 17:16:11 +0400

    add peerDep

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,54 +1,49 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/Button';
+import * as React from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function App() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-    reset,
-  } = useForm({
-    mode: 'onBlur',
-  });
+  // const {
+  //   register,
+  //   formState: { errors },
+  //   handleSubmit,
+  //   reset,
+  // } = useForm({
+  //   mode: 'onBlur',
+  // });
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-    reset();
-  };
+  // const onSubmit = (data) => {
+  //   alert(JSON.stringify(data));
+  //   reset();
+  // };
 
   return (
-    <div>
-      <h1>Form</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          First Name:
-          <input
-            {...register('firstName', {
-              required: 'обязательно к заполнению',
-              minLength: { value: 5, message: 'минимум 5 символов' },
-            })}
-          />
-        </label>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+        />
 
-        <div style={{ height: 40 }}>
-          {errors?.firstName && <p>{errors?.firstName.message || 'Error!'}</p>}
-        </div>
-
-        <label>
-          Last Name:
-          <input
-            {...register('lastName', {
-              required: 'обязательно к заполнению',
-              minLength: { value: 5, message: 'минимум 5 символов' },
-            })}
-          />
-        </label>
-
-        <div style={{ height: 40 }}>
-          {errors?.lastName && <p>{errors?.lastName.message || 'Error!'}</p>}
-        </div>
-
-        <input type="submit" />
-      </form>
-    </div>
+        <TextField
+          id="outlined-read-only-input"
+          label="Read Only"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+    </Box>
   );
 }
