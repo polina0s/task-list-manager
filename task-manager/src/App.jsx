4c30e737d2db082ba9e5385e05c38b0ab5133922
polina0s <polina.osintseva000@gmail.com>
2023-08-31 17:33:31 +0400

    add styled components for mui

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,22 +1,21 @@
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/Button';
-import * as React from 'react';
+import TextField from '@mui/material/TextField';
 import { useForm } from 'react-hook-form';
 
 export default function App() {
-  // const {
-  //   register,
-  //   formState: { errors },
-  //   handleSubmit,
-  //   reset,
-  // } = useForm({
-  //   mode: 'onBlur',
-  // });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: 'onBlur',
+  });
 
-  // const onSubmit = (data) => {
-  //   alert(JSON.stringify(data));
-  //   reset();
-  // };
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
+  };
 
   return (
     <Box
@@ -28,22 +27,51 @@ export default function App() {
       autoComplete="off"
     >
       <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
+        <TextField required id="outlined" label="Login" defaultValue="login" />
 
         <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
+          required
+          id="outlined"
+          label="Password"
+          defaultValue="password"
         />
       </div>
     </Box>
   );
 }
+
+// <div>
+//   <h1>Form</h1>
+//   <form onSubmit={handleSubmit(onSubmit)}>
+//     <label>
+//       First Name:
+//       <TextField label="Outlined secondary" color="secondary" />
+//       <input
+//         {...register('firstName', {
+//           required: 'обязательно к заполнению',
+//           minLength: { value: 5, message: 'минимум 5 символов' },
+//         })}
+//       />
+//     </label>
+
+//     <div style={{ height: 40 }}>
+//       {errors?.firstName && <p>{errors?.firstName.message || 'Error!'}</p>}
+//     </div>
+
+//     <label>
+//       Last Name:
+//       <input
+//         {...register('lastName', {
+//           required: 'обязательно к заполнению',
+//           minLength: { value: 5, message: 'минимум 5 символов' },
+//         })}
+//       />
+//     </label>
+
+//     <div style={{ height: 40 }}>
+//       {errors?.lastName && <p>{errors?.lastName.message || 'Error!'}</p>}
+//     </div>
+
+//     <input type="submit" />
+//   </form>
+// </div>
