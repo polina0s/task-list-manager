f388feb27f1713b753b31fdfc005969f85e72dbb
polina0s <polina.osintseva000@gmail.com>
2023-09-12 15:49:34 +0400

    add react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { configureStore } from '@reduxjs/toolkit';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const defaultState = {
+  cash: 0,
+};
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      return { ...state, cash: state.cash + action.payload };
+    case 'GET_CASH':
+      return { ...state, cash: state.cash - action.payload };
+    default:
+      return state;
+  }
+};
+
+const store = configureStore(reducer);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider>
+    <App />
+  </Provider>,
+);
