5657a81981c3dfd4442aec61b5c46cd9076f3f77
polina0s <polina.osintseva000@gmail.com>
2023-09-27 17:04:05 +0400

    add lazy loading

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,45 +1,58 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import { AuthLayout } from '../layouts/auth-layout';
-import { RootLayout } from '../layouts/root-layout';
-import { UnauthLayout } from '../layouts/unauth-layout';
-import { ErrorPage } from '../pages/error-page';
-import { HomePage } from '../pages/home-page';
-import { Login } from '../pages/login/login';
-import { Registration } from '../pages/registration';
-
 export const Router = createBrowserRouter([
   {
     path: '/',
-    element: <RootLayout />,
+    async lazy() {
+      let { RootLayout } = await import('../layouts/root-layout');
+      return { Component: RootLayout };
+    },
     children: [
       {
         path: '/home',
-        element: <AuthLayout />,
+        async lazy() {
+          let { AuthLayout } = await import('../layouts/auth-layout');
+          return { Component: AuthLayout };
+        },
         children: [
           {
             path: '',
-            element: <HomePage />,
+            async lazy() {
+              let { HomePage } = await import('../pages/home-page');
+              return { Component: HomePage };
+            },
           },
         ],
       },
       {
-        element: <UnauthLayout />,
+        async lazy() {
+          let { UnauthLayout } = await import('../layouts/unauth-layout');
+          return { Component: UnauthLayout };
+        },
         path: '/',
         children: [
           {
             path: '/',
-            element: <Registration />,
+            async lazy() {
+              let { Registration } = await import('../pages/registration');
+              return { Component: Registration };
+            },
           },
         ],
       },
       {
         path: '/login',
-        element: <Login />,
+        async lazy() {
+          let { Login } = await import('../pages/login');
+          return { Component: Login };
+        },
       },
       {
         path: 'error',
-        element: <ErrorPage />,
+        async lazy() {
+          let { ErrorPage } = await import('../pages/error-page');
+          return { Component: ErrorPage };
+        },
       },
     ],
   },
