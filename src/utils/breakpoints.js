0915223956b6c350c24a6f00b199ffc9bf5ae1d8
polina0s <polina.osintseva000@gmail.com>
2023-09-27 15:04:39 +0400

    fixed useBreakpoints func

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,13 +1,23 @@
 import { useTheme } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
 
 export function useBreakpoints() {
   const theme = useTheme();
-  const size = window.innerWidth;
 
-  const xl = size <= theme.breakpoints.values.xl;
-  const lg = size <= theme.breakpoints.values.lg;
-  const md = size <= theme.breakpoints.values.md;
-  const sm = size <= theme.breakpoints.values.sm;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resize = () => setWidth(window.innerWidth);
+
+    window.addEventListener('resize', resize);
+
+    return () => window.removeEventListener('resize', resize);
+  }, []);
+
+  const xl = width <= theme.breakpoints.values.xl;
+  const lg = width <= theme.breakpoints.values.lg;
+  const md = width <= theme.breakpoints.values.md;
+  const sm = width <= theme.breakpoints.values.sm;
 
   return {
     xl,
