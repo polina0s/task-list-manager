0caaa55c4fd44dabc986f75d26226a817930b670
polina0s <polina.osintseva000@gmail.com>
2023-09-28 16:20:23 +0400

    add delete icon for task comp

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,4 +1,6 @@
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { IconButton } from '@mui/material';
 
 import { Title } from '../title';
 import task from './task.module.scss';
@@ -7,8 +9,15 @@ export function Task({ name }) {
   return (
     <div className={task.cont}>
       <div className={task.titleCont}>
-        <AutoAwesomeIcon className={task.titleIcon} color="secondary" />
-        <Title name={name} color="secondary" />
+        <div className={task.nameCont}>
+          <AutoAwesomeIcon className={task.titleIcon} color="secondary" />
+          <Title name={name} color="secondary" />
+        </div>
+        <div>
+          <IconButton>
+            <DeleteOutlineIcon color="secondary" />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
