5839bdc7f4bdd55008768b594224fbd07891156c
polina0s <polina.osintseva000@gmail.com>
2023-10-03 17:42:28 +0400

    added logic for opening a task form

--- src/components/task-form/taskForm.jsx
+++ src/components/task-form/taskForm.jsx
@@ -1,8 +1,8 @@
 // import Button from '@mui/material/Button';
-// import Modal from '@mui/material/Modal';
 import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
 import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 
 import { Button } from '../button/button';
 import { Input } from '../input/input';
@@ -10,27 +10,25 @@ import { Title } from '../title';
 // import * as React from 'react';
 import form from './taskForm.module.scss';
 
-export function TaskForm() {
+export function TaskForm({ onCloseClick, open }) {
   // const [open, setOpen] = React.useState(false);
   // const handleOpen = () => setOpen(true);
   // const handleClose = () => setOpen(false);
   return (
     <div>
       {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      {/* <Modal */}
-      {/* open={open} onClose={handleClose} */}
-      {/* > */}
-      <Box className={form.cont}>
-        <div className={form.titleCont}>
-          <Title color="secondary" variant="h6" name="Create task" />
-          <IconButton>
-            <CloseIcon color="secondary" />
-          </IconButton>
-        </div>
-        <Input className={form.input} label="task text" />
-        <Button name="Add task" />
-      </Box>
-      {/* </Modal> */}
+      <Modal open={open} onClose={onCloseClick}>
+        <Box className={form.cont}>
+          <div className={form.titleCont}>
+            <Title color="secondary" variant="h6" name="Create task" />
+            <IconButton onClick={onCloseClick}>
+              <CloseIcon color="secondary" />
+            </IconButton>
+          </div>
+          <Input className={form.input} label="task text" />
+          <Button name="Add task" />
+        </Box>
+      </Modal>
     </div>
   );
 }
--- src/widgets/task-board/taskBoard.jsx
+++ src/widgets/task-board/taskBoard.jsx
@@ -1,21 +1,34 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { useState } from 'react';
 
 import { TaskForm } from '../../components/task-form';
 import { TaskList } from '../../components/task-list';
 import board from './taskBoard.module.scss';
 
 export function TaskBoard() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Box className={board.cont}>
         <Grid container spacing={2}>
-          <TaskList name="to do" id="toDo" />
-          <TaskList name="in progress" id="inProgress" />
-          <TaskList name="done" id="done" />
+          <TaskList onAddClick={handleOpen} name="to do" id="toDo" />
+          <TaskList
+            onAddClick={handleOpen}
+            name="in progress"
+            id="inProgress"
+          />
+          <TaskList onAddClick={handleOpen} name="done" id="done" />
         </Grid>
       </Box>
-      <TaskForm />
+      <TaskForm onCloseClick={handleClose} open={open} />
     </>
   );
 }
