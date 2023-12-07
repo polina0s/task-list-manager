a01330da53187d6ac946da7456e8b8340929f2b3
polina0s <polina.osintseva000@gmail.com>
2023-12-07 19:20:14 +0400

    fixed deleteTag thunk

--- src/pages/task-page/taskPage.jsx
+++ src/pages/task-page/taskPage.jsx
@@ -79,7 +79,7 @@ export function TaskPage() {
   };
 
   const handleDeleteTagFromList = () => {
-    dispatch(deleteTag({ id: idTag }))
+    dispatch(deleteTag({ id: idTag, dispatch: dispatch }))
       .unwrap()
       .then(() => {
         handleCloseConfirmModal();
