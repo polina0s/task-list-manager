0362267c7fc9d1af2511205a41b4eb731eb0699a
polina0s <polina.osintseva000@gmail.com>
2023-12-12 18:11:24 +0400

    replaced fetch to axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import queryString from 'query-string';
 
 import { tokenService } from '../utils/tokenService';
@@ -9,28 +10,43 @@ export class Api {
     const { access, refresh } = tokenService.getTokens();
     this.access = access;
     this.refresh = refresh;
-  }
 
-  async request(url, options = {}, repeat = true) {
-    const response = await fetch(`${this.api}/${url}`, {
-      ...options,
+    this.instance = axios.create({
+      baseURL: this.api,
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${this.access}`,
-        ...options?.headers,
       },
-      body: options.body ? JSON.stringify(options.body) : undefined,
     });
 
-    if (response.status === 401) {
-      if (repeat) {
-        this.queue.push(() => this.request(url, options, false));
-        this.onRefresh();
+    this.instance.interceptors.request.use(function (response) {
+      
+      if (response.status === 401) {
+        this.queue.push(() => this.instance);
       }
-    }
-
-    return response;
+    });
   }
+
+  // async request(url, options = {}, repeat = true) {
+  //   const response = await fetch(`${this.api}/${url}`, {
+  //     ...options,
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${this.access}`,
+  //       ...options?.headers,
+  //     },
+  //     body: options.body ? JSON.stringify(options.body) : undefined,
+  //   });
+
+  //   if (response.status === 401) {
+  //     if (repeat) {
+  //       this.queue.push(() => this.request(url, options, false));
+  //       this.onRefresh();
+  //     }
+  //   }
+
+  //   return response;
+  // }
 
   setTokens(access, refresh) {
     tokenService.saveTokens({
@@ -43,55 +59,48 @@ export class Api {
   }
 
   async createUser({ login, password }) {
-    const response = await this.request('users', {
-      method: 'POST',
-      body: { login, password },
+    const response = await this.instance.post('users', {
+      login,
+      password,
     });
 
-    const json = await response.json();
+    this.setTokens(
+      response.data.data.accessToken,
+      response.data.data.refreshToken,
+    );
 
-    if (!response.ok) {
-      throw new Error(json.error);
-    }
-
-    this.setTokens(json.data.accessToken, json.data.refreshToken);
-
-    return json;
+    return response.data;
   }
 
   async loginUser({ login, password }) {
-    const response = await this.request('auth/login', {
-      method: 'POST',
-      body: { login, password },
+    const response = await this.instance.post('auth/login', {
+      login,
+      password,
     });
 
-    const json = await response.json();
+    this.setTokens(
+      response.data.data.accessToken,
+      response.data.data.refreshToken,
+    );
 
-    if (!response.ok) {
-      throw new Error(json.error);
-    }
-
-    this.setTokens(json.data.accessToken, json.data.refreshToken);
-
-    return json;
+    return response.data;
   }
 
   async fetchUserById({ id }) {
-    const response = await this.request(`users/${id}`);
-    const json = await response.json();
+    const response = await this.instance.get(`users/${id}`);
 
-    return json;
+    return response.data;
   }
 
   async refreshTokens() {
-    const response = await this.request('auth/refresh', {
-      method: 'POST',
-      body: { refreshToken: this.refresh },
+    const response = await this.instance.post('auth/refresh', {
+      refreshToken: this.refresh,
     });
 
-    const json = await response.json();
-
-    this.setTokens(json.data.accessToken, json.data.refreshToken);
+    this.setTokens(
+      response.data.data.accessToken,
+      response.data.data.refreshToken,
+    );
   }
 
   clearQueue() {
@@ -107,88 +116,63 @@ export class Api {
   }
 
   async createTask({ text, tags = [] }) {
-    const response = await this.request('tasks', {
-      method: 'POST',
-      body: { text, tags },
+    const response = await this.instance.post('tasks', {
+      text,
+      tags,
     });
 
-    const json = await response.json();
+    console.log(response);
 
-    return json;
+    return response.data;
   }
 
   async getTasks({ offset = 0, limit = 10 }) {
     const query = queryString.stringify({ limit, offset }, { skipNull: true });
 
-    const response = await this.request(`tasks?${query}`, { method: 'GET' });
+    const response = await this.instance.get(`tasks?${query}`);
 
-    const json = await response.json();
-
-    return json;
+    return response.data;
   }
 
   async editTask({ id, text, tags }) {
-    const response = await this.request(`tasks/${id}`, {
-      method: 'PATCH',
-      body: {
-        text,
-        tags,
-      },
-    });
+    const response = await this.instance.patch(`tasks/${id}`, { text, tags });
 
-    const json = await response.json();
-
-    return json;
+    return response.data;
   }
 
   async deleteTask({ id }) {
-    await this.request(`tasks/${id}`, { method: 'DELETE' });
+    await this.instance.delete(`tasks/${id}`);
   }
 
   async getTags({ offset = 0, limit = 10 }) {
     const query = queryString.stringify({ limit, offset }, { skipNull: true });
 
-    const response = await this.request(`tags?${query}`, { method: 'GET' });
+    const response = await this.instance.get(`tags?${query}`);
 
-    const json = await response.json();
-
-    return json;
+    return response.data;
   }
 
   async createTag({ name, color }) {
-    const response = await this.request('tags', {
-      method: 'POST',
-      body: {
-        name: name,
-        color: color,
-      },
+    const response = await this.instance.post('tags', {
+      name,
+      color,
     });
 
-    const json = await response.json();
-
-    return json;
+    return response.data;
   }
 
   async editTag({ name, color, id }) {
-    const response = await this.request(`tags/${id}`, {
-      method: 'PATCH',
-      body: { name: name, color: color },
-    });
+    const response = await this.instance.patch(`tags/${id}`, { name, color });
 
-    const json = await response.json();
-
-    return json;
+    return response.data;
   }
 
   async deleteTag({ id }) {
-    await this.request(`tags/${id}`, { method: 'DELETE' });
+    await this.instance.delete(`tags/${id}`);
   }
 
   async changeTaskStatus({ id, status }) {
-    this.request(`tasks/${id}/status`, {
-      method: 'PATCH',
-      body: { status: status },
-    });
+    await this.instance.patch(`tasks/${id}/status`, { status });
   }
 
   onRefresh() {}
